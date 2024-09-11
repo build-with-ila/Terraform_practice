8e1bbab3aa4c01ee1488dd467a37d4856fa30c2d
codecanvas <codecanvas@hotmail.com>
2024-09-11 21:05:54 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami_val"{
+    description = "ami value"
+    type = string
+}
+variable "instance_type_val"{
+    description = "instance type value"
+    type = string
+}
+variable "subnet_id_val"{
+    description = "subnet id value"
+    type = string
+}
